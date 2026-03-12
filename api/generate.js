ebadd6c2381ec918fac5c2e326105e4c57b30098
Manu Barki <manulb9@gmail.com>
2026-03-12 12:05:23 +0530

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         "X-Title": "SourcingCompass",
       },
       body: JSON.stringify({
-        model: "openrouter/auto",
+        model: "openrouter/free",
         messages: [{ role: "user", content: prompt }],
         temperature: 0.7,
         max_tokens: 4000,
